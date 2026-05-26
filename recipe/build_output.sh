abf481888a9c3768f605f1f79a12d0c973bbbbac
Andres Guzman-Ballen <acosmicflamingo@gmail.com>
2026-05-26 11:54:14 -0500

    Bump python minimum requirement; add CUDA_MAJOR debug info

--- recipe/build_output.sh
+++ recipe/build_output.sh
@@ -4,7 +4,7 @@ export NVCC="$(which nvcc)"
 if [ "${CUDA_MAJOR}" -ge 12 ]; then
     export CUDA_PATH=$PREFIX/targets/${TARGET_NAME}
 fi
-echo "nvcc is $NVCC, CUDA path is $CUDA_PATH"
+echo "nvcc is $NVCC, CUDA path is $CUDA_PATH, CUDA major version is $CUDA_MAJOR"
 echo "CONDA_OVERRIDE_CUDA is $CONDA_OVERRIDE_CUDA"
 
 ${PYTHON} -m pip install . --no-deps -vv
